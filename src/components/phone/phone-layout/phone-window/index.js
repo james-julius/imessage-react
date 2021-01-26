de48a42a8a46441934aef7f4f28ff4f43ea44d36
james-julius <jamesdarby7@gmail.com>
2021-01-25 17:09:48 -0800

    Laid foundation for iframe comms. Will this work? Lets find out

--- src/components/phone/phone-layout/phone-window/index.js
+++ src/components/phone/phone-layout/phone-window/index.js
@@ -1,30 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../style.module.css'
 import Header from "../../header";
 import CollapsedInputFooter from "../../footer/collapsed-input-footer";
 import MessageHistory from "../../messages/message-history";
 
 const PhoneWindow = () => {
-  let spotifyURI = "https://open.spotify.com/track/3PFnF7lmHOxoYGXhxUkzCE?si=Kjoy_nseQ_2-OxGTRQvTag";
-  let messages = [
-    {
-      isArtist: true,
-      text: `How's isolation been for you?`,
-    },
-    {
-      isArtist: false,
-      text: `Not too bad thanks`,
-    },
-    {
-      isArtist: true,
-      text: `Have you been making lots of music? `,
-    },
-    {
-      isArtist: false,
-      text: `Oh yeah`,
-    },
-  ];
-  let name = 'James Julius';
+  // Get ready to assign this data
+  window.addEventListener('message', function(event) {
+    var origin = event.origin || event.originalEvent.origin; // For Chrome, the origin property is in the event.originalEvent object.
+    if (typeof event.data == 'object' && event.data.call=='sendValue') {
+        let data = event.data.value;
+        setSpotifyURI(data.spotifyURI);
+        setMessages(data.messages);
+        setName(data.name);
+    }
+}, false);
+
+  
+  document.onload = function() {
+    window.parent.postMessage('message', () => 'readyForProps')
+  }
+
+  const [spotifyURI, setSpotifyURI] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [name, setName] = useState('');
+
+  // let spotifyURI = "https://open.spotify.com/track/3PFnF7lmHOxoYGXhxUkzCE?si=Kjoy_nseQ_2-OxGTRQvTag";
+  // let messages = [
+  //   {
+  //     isArtist: true,
+  //     text: `How's isolation been for you?`,
+  //   },
+  //   {
+  //     isArtist: false,
+  //     text: `Not too bad thanks`,
+  //   },
+  //   {
+  //     isArtist: true,
+  //     text: `Have you been making lots of music? `,
+  //   },
+  //   {
+  //     isArtist: false,
+  //     text: `Oh yeah`,
+  //   },
+  // ];
+  // let name = 'James Julius';
   function initialiseName() {
     let splitName = name.split(' ');
     return splitName[0][0].toUpperCase() + splitName[1][0].toUpperCase();
