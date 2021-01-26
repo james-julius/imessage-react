b5fe639bfb8f8e3fb95556d0071cb8af936bace6
james-julius <jamesdarby7@gmail.com>
2021-01-25 20:50:26 -0800

    Setup alert for receiving message in iframe

--- src/components/phone/phone-layout/phone-window/index.js
+++ src/components/phone/phone-layout/phone-window/index.js
@@ -7,6 +7,7 @@ import MessageHistory from "../../messages/message-history";
 const PhoneWindow = () => {
   // Get ready to assign this data
   window.addEventListener('message', function(event) {
+    alert(event);
     // var origin = event.origin || event.originalEvent.origin; // For Chrome, the origin property is in the event.originalEvent object.
     if (typeof event.data === 'object' && event.data.call === 'sendValue') {
         let data = event.data.value;
@@ -24,27 +25,7 @@ const PhoneWindow = () => {
   const [spotifyURI, setSpotifyURI] = useState('');
   const [messages, setMessages] = useState([]);
   const [name, setName] = useState('R R');
-
-  // let spotifyURI = "https://open.spotify.com/track/3PFnF7lmHOxoYGXhxUkzCE?si=Kjoy_nseQ_2-OxGTRQvTag";
-  // let messages = [
-  //   {
-  //     isArtist: true,
-  //     text: `How's isolation been for you?`,
-  //   },
-  //   {
-  //     isArtist: false,
-  //     text: `Not too bad thanks`,
-  //   },
-  //   {
-  //     isArtist: true,
-  //     text: `Have you been making lots of music? `,
-  //   },
-  //   {
-  //     isArtist: false,
-  //     text: `Oh yeah`,
-  //   },
-  // ];
-  // let name = 'James Julius';
+x
   function initialiseName() {
     let splitName = name.split(' ');
     return splitName[0][0].toUpperCase() + splitName[1][0].toUpperCase();
